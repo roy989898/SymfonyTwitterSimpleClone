c4809cd80be0926033408b50747e111469acf7fc
pom <roy989898@gmail.com>
2022-12-15 20:21:28 +0000

    add repeat password

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -27,11 +28,20 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
+            ->add("plainPassword", RepeatedType::class, [
+
+                'type' => PasswordType::class,
+                'invalid_message' => 'The password fields must match',
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
+                'first_options' => [
+                    'label' => 'Password',
+                    'mapped' => false
+                ],
+                'second_options' => [
+                    'label' => 'Repeated Password',
+                    'mapped' => false
+                ],
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter a password',
@@ -43,8 +53,25 @@ class RegistrationFormType extends AbstractType
                         'max' => 4096,
                     ]),
                 ],
-            ])
-        ;
+
+            ]);
+        /*     ->add('plainPassword', PasswordType::class, [
+                 // instead of being set onto the object directly,
+                 // this is read and encoded in the controller
+                 'mapped' => false,
+                 'attr' => ['autocomplete' => 'new-password'],
+                 'constraints' => [
+                     new NotBlank([
+                         'message' => 'Please enter a password',
+                     ]),
+                     new Length([
+                         'min' => 6,
+                         'minMessage' => 'Your password should be at least {{ limit }} characters',
+                         // max length allowed by Symfony for security reasons
+                         'max' => 4096,
+                     ]),
+                 ],
+             ]);*/
     }
 
     public function configureOptions(OptionsResolver $resolver): void
