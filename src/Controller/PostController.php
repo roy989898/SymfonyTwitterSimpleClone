ce7bbf76899f6ca48650e1f1be65c40217b165af
pom <roy989898@gmail.com>
2022-12-17 18:16:56 +0000

    can edit post

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -48,4 +48,30 @@ class PostController extends AbstractController
         return $this->render('post/create.html.twig', ['form' => $form]);
 
     }
+
+
+    #[isGranted('ROLE_USER')]
+    #[Route('/post/edit/{post<\d+>}', name: 'app_post_edit')]
+    public function editPost(Post $post, Request $request): Response
+    {
+        $postUserID = $post->getUser()?->getId();
+        $userID = $this->getUser()?->getId();
+        if ($postUserID !== $userID) {
+            $this->redirectToRoute('app_post');
+        }
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            /**@var $formPost Post */
+            $formPost = $form->getData();
+
+            $this->postRepository->save($formPost, true);
+
+        }
+
+        return $this->render('post/edit.html.twig', ['form' => $form]);
+
+    }
+
+
 }
