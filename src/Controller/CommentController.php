480fd5c681b3aacbd395693f4f7d62c6aa4adf2d
pom <roy989898@gmail.com>
2022-12-17 22:48:27 +0000

    can update comment

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -2,17 +2,38 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
+use App\Form\CommentFormType;
+use App\Repository\CommentRepository;
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CommentController extends AbstractController
 {
-    #[Route('/comment/edit', name: 'app_comment_edit')]
-    public function edit(): Response
+    public function __construct(private CommentRepository $commentRepository, private PostRepository $postRepository)
     {
+    }
+
+    #[Route('/comment/edit/{comment<\d+>}', name: 'app_comment_edit')]
+    public function edit(Comment $comment, Request $request): Response
+    {
+
+        $commentForm = $this->createForm(CommentFormType::class, $comment);
+        $commentForm->handleRequest($request);
+        if ($commentForm->isSubmitted() && $commentForm->isValid() && $comment->getUser()?->getId() !== null && ($this->getUser()?->getID() === $comment->getUser()?->getId())) {
+            /**@var $formComment Comment */
+            $formComment = $commentForm->getData();
+            $this->commentRepository->save($formComment, true);
+
+            return $this->redirectToRoute('app_post_detail', ['post' => $comment->getPost()?->getId()]);
+
+//            TODO redirect to where???
+        }
         return $this->render('comment/edit.html.twig', [
-            'controller_name' => 'CommentController',
+            'commentForm' => $commentForm
         ]);
     }
 }
