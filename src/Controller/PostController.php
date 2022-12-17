0e548ead6823060cb7cd0265053ad242a0dadccb
pom <roy989898@gmail.com>
2022-12-17 15:50:22 +0000

    add a simple top bar

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PostController extends AbstractController
 {
+
+    public function __construct(private PostRepository $postRepository)
+    {
+    }
+
     #[Route('/post', name: 'app_post')]
     public function index(): Response
     {
+        $posts = $this->postRepository->findAll();
         return $this->render('post/index.html.twig', [
-            'controller_name' => 'PostController',
+            'posts' => $posts
         ]);
     }
 }
