74a4f227a809779ad2ee914e032efbe2fee4d2d8
pom <roy989898@gmail.com>
2022-12-17 14:47:06 +0000

    add role_hierarchy

--- src/Controller/TestingController.php
+++ src/Controller/TestingController.php
@@ -3,11 +3,18 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class TestingController extends AbstractController
 {
+
+    public function __construct(private Security $security)
+    {
+    }
+
     #[Route('/testing', name: 'app_testing')]
     public function index(): Response
     {
@@ -17,9 +24,11 @@ class TestingController extends AbstractController
     }
 
 
+    #[isGranted('ROLE_USER')]
     #[Route('/testingProtected', name: 'app_testing')]
     public function testingProtected(): Response
     {
+
         return $this->render('testing/protected.html.twig', [
 
         ]);
