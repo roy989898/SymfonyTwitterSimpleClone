3d6e0ab2b4332798a23e6e1dcc9b4097c2884d57
pom <roy989898@gmail.com>
2022-12-17 17:07:22 +0000

    temp save

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use App\Form\PostType;
 use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -21,5 +24,24 @@ class PostController extends AbstractController
         return $this->render('post/index.html.twig', [
             'posts' => $posts
         ]);
+    }
+
+    #[Route('/post/add', name: 'app_post_add')]
+    public function addPost(Request $request)
+    {
+        $post = new Post();
+
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            /**@var $formPost Post */
+            $formPost = $form->getData();
+            $this->postRepository->save($formPost, true);
+
+        }
+
+//        TODO add the the form
+        return $this->render('post/create.html.twig');
+
     }
 }
